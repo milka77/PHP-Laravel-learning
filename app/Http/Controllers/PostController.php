d37db0f7ae8ec9e530189d263e4e2244a70acc61
milka77 <55999046+milka77@users.noreply.github.com>
2021-06-14 08:51:09 +0100

    Advanced validation with CreatePostRequest.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\CreatePostRequest;
 use App\Models\Post;
 
 class PostController extends Controller
@@ -40,13 +41,13 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreatePostRequest $request)
     {
         // Validation
-        $this->validate($request, [
-            'title'=>'required',
-            'body'=>'required'
-        ]);
+        // $this->validate($request, [
+        //     'title'=>'required',
+        //     'body'=>'required'
+        // ]);
 
         // return $request->all();
         // 1st way
